451e71ceb0412782ac9588966610bacb0052724f
Rob Ranf <rob@emiyaconsulting.com>
2025-07-20 18:22:46 -0700

    transfer service function start and tf sqs create

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { copyS3ToGCS } from "./functions/copyS3ToGCS.js";
 
+const awsS3Bucket = process.env.S3_BUCKET_FILES
+const gcsStorageBucket = process.env.GCS_BUCKET
+const gcsBucketPath = process.env.GCS_DEST_PATH
+
+
 async function handler(
-	s3Bucket: string,
-	gcsBucket: string,
-	gcsDestPath: string
+	s3Bucket: string = awsS3Bucket!,
+	gcsBucket: string = gcsStorageBucket!,
+	gcsDestPath: string = gcsBucketPath!
 ) {
 	copyS3ToGCS(s3Bucket, gcsBucket, gcsDestPath);
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -37,3 +37,40 @@ resource "aws_s3_bucket_lifecycle_configuration" "archived_files_delete_30_days"
     }
   }
 }
+
+# Create an SQS queue to notify GCS of a new file
+resource "aws_sqs_queue" "file_transfer_to_gcs_queue" {
+  name                      = "emiya-todo-aws-to-gcs-file-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Environment = "dev"
+  }
+
+  policy = {
+    "Version" : "2012-10-17",
+    "Id" : "example-ID",
+    "Statement" : [
+      {
+        "Sid" : "example-statement-ID",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "s3.amazonaws.com"
+        },
+        "Action" : "SQS:SendMessage",
+        "Resource" : "RESOURCE",
+        "Condition" : {
+          "StringEquals" : {
+            "aws:SourceAccount" : "AWS"
+          },
+          "ArnLike" : {
+            "aws:SourceArn" : "S3_BUCKET_ARN"
+          }
+        }
+      }
+    ]
+  }
+}
